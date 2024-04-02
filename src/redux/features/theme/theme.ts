a63ef2418d845fe32392ff20ac34d0aed8d1a8d9
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2024-04-03 05:34:09 +0600

    theme set on local storage

--- src/redux/features/theme/theme.ts
+++ src/redux/features/theme/theme.ts
@@ -1,18 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit"
 
+import { createSlice } from "@reduxjs/toolkit";
+
+const storedDarkMode = localStorage.getItem("darkMode");
 const initialState = {
-    darkMode: false
-}
+  darkMode: storedDarkMode ? JSON.parse(storedDarkMode) : false
+};
 
 const themeSlice = createSlice({
-    name: "theme", 
-    initialState, 
-    reducers: {
-        toggleTheme : (state) => {
-            state.darkMode = !state.darkMode
-        }
+  name: "theme",
+  initialState,
+  reducers: {
+    toggleTheme: (state) => {
+      state.darkMode = !state.darkMode;
+      localStorage.setItem("darkMode", JSON.stringify(state.darkMode));
     }
-})
+  }
+});
 
-export const {toggleTheme} = themeSlice.actions;
+export const { toggleTheme } = themeSlice.actions;
 export default themeSlice.reducer;
+
